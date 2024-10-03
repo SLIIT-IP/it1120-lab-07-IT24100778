063c86935f0386c0c809f7b18ffff04f0949d4bb
IT24100778 <it24100778@my.sliit.lk>
2024-10-03 12:57:47 +0530

    IT24100778Lab7Q2A.java & IT24100778Lab7Q2B.java

---
+++ Q2/IT24100778Lab7Q2A.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ public class IT24100778Lab7Q2A{
+  public static void main (String[]args){
+     Scanner input = new Scanner (System.in);
+	  System.out.println("$ $ $ $ $");
+	    System.out.println("$ $ $ $ $");
+		  System.out.println("$ $ $ $ $");
+		    System.out.println("$ $ $ $ $");
+	}		
+}	
---
+++ Q2/IT24100778Lab7Q2B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class IT24100778Lab7Q2B{
+  public static void main (String[]args){
+     Scanner input = new Scanner (System.in);
+	  System.out.println("1 - *");
+	    System.out.println("2 - * *");
+		  System.out.println("3 - * * *");
+		    System.out.println("4 - * * * *");
+			 System.out.println("5 - * * * * *");
+	}		
+}	
