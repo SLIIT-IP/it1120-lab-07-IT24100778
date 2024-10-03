4f46cf7495154e6cee16439893d1e5a209524a42
IT24100778 <it24100778@my.sliit.lk>
2024-10-03 12:58:28 +0530

    IT24100778Lab7Q2C.java

---
+++ Q2/IT24100778Lab7Q2C.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ public class IT24100778Lab7Q2C{
+  public static void main (String[]args){
+     Scanner input = new Scanner (System.in);
+	  System.out.println("55555");
+	    System.out.println("4444");
+		  System.out.println("333");
+		    System.out.println("22");
+			 System.out.println("1");
+	}		
+}	
